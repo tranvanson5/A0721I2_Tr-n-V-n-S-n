91c4c35badc2696f621636a148f280861b1aa027
Trần Văn Sơn <87640431+tranvanson5@users.noreply.github.com>
2021-10-01 17:44:55 +0700

    up

---
+++ Module2/java/src/bai_1/ThucHanh/B1.java
@@ -0,0 +1,8 @@
+package bai_1.ThucHanh;
+
+public class B1 {
+    public static void main(String[] agrs){
+        System.out.println("hello");
+    }
+
+}
---
+++ Module2/java/src/bai_1/ThucHanh/ThoiGian.java
@@ -0,0 +1,10 @@
+package bai_1.ThucHanh;
+
+import java.util.Date;
+
+public class ThoiGian {
+    public static void main(String[] args) {
+       Date now= new Date();
+        System.out.println("Date now "+now);
+    }
+}
