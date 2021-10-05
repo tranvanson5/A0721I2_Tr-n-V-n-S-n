98ffda171cd0c366920d22ce307773fba157536c
Trần Văn Sơn <87640431+tranvanson5@users.noreply.github.com>
2021-10-06 01:57:56 +0700

    up

--- Module2/java/src/introduction_to_java/baitap/DocSoThanhChu.java
+++ Module2/java/src/introduction_to_java/baitap/DocSoThanhChu.java
@@ -1,12 +1,7 @@
 package introduction_to_java.baitap;
 
 public class DocSoThanhChu {
-    static void dvTram(int n){
-    }
     public static void main(String[] args) {
-        for(int i=1; i<=5; i++){
-            if(i==1) break;
-            System.out.println("Hello");
-        }
+
     }
 }
